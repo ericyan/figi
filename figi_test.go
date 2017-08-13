cc84a8d65eb4383757fcc526944a42c64fbc0ff9
Eric Yan <github@ericyan.me>
2017-08-13 15:23:29 +0800

    Add exmaple for API client

--- figi_test.go
+++ figi_test.go
@@ -1,6 +1,9 @@
 package figi
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestTextMarshalling(t *testing.T) {
 	cases := []struct {
@@ -30,4 +33,52 @@ func TestTextMarshalling(t *testing.T) {
 			}
 		}
 	}
+}
+
+func ExampleClient() {
+	req := []MappingRequest{
+		{
+			IDType:  "ID_EXCH_SYMBOL",
+			IDValue: "5",
+			MIC:     "XHKG",
+		},
+		{
+			IDType:  "ID_SEDOL",
+			IDValue: "3070732",
+		},
+		{
+			IDType:  "ID_ISIN",
+			IDValue: "XS0202077953",
+		},
+		{
+			IDType:  "ID_CUSIP",
+			IDValue: "BOGUSCUSIP",
+		},
+	}
+
+	c := NewClient()
+	resp, err := c.Query(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for i, mapping := range resp {
+		if mapping.Success() {
+			for _, result := range mapping.Data {
+				fmt.Printf("%s => %s: %s\n", req[i].IDValue, result.FIGI, result.Name)
+			}
+		} else {
+			fmt.Printf("%s => ERROR: %s\n", req[i].IDValue, mapping.Error)
+		}
+	}
+
+	// Output:
+	// 5 => BBG000BQ9HJ2: HSBC HOLDINGS PLC
+	// 3070732 => BBG000BT7PY3: BANK OF IRELAND
+	// 3070732 => BBG000FXQJP3: BANK OF IRELAND
+	// 3070732 => BBG000G7GBS4: BANK OF IRELAND
+	// 3070732 => BBG000JP1BD3: BANK OF IRELAND
+	// 3070732 => BBG0025BSC99: BANK OF IRELAND
+	// XS0202077953 => BBG0000585F1: WAL-MART STORES INC
+	// BOGUSCUSIP => ERROR: No identifier found.
 }
